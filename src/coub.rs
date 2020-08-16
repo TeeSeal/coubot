728b53be73a04f4785ad44091887b60920cd3287
Alex Hincu <teeseal@sorta.moe>
2020-08-16 16:10:13 +0300

    feat: migrate everything to async rust

--- src/coub.rs
+++ src/coub.rs
@@ -1,10 +1,11 @@
-use std::process::Command;
+use bytes::{BufMut, BytesMut};
 use serde_json::Value;
-use std::{error::Error, io::Write, path::Path};
+use std::{error::Error, path::Path};
 use tempfile::NamedTempFile;
+use tokio::{fs::File, prelude::*, process::Command};
 use url::Url;
 
-type BoxResult<T> = Result<T, Box<dyn Error>>;
+type BoxResult<T> = Result<T, Box<dyn Error + Send + Sync>>;
 
 pub struct Coub {
     pub id: String,
@@ -12,75 +13,77 @@ pub struct Coub {
     pub video: String,
     pub audio: String,
     pub duration: f64,
-    pub size: u64
+    pub size: u64,
 }
 
 impl Coub {
-    pub fn download(&self, path: &Path) -> BoxResult<()> {
-        let mut video = NamedTempFile::new()?;
-        self.write_video_to(video.as_file_mut())?;
+    pub async fn download(&self, path: &Path) -> BoxResult<()> {
+        let video_path = NamedTempFile::new()?.into_temp_path();
+        self.write_video_to(&video_path).await?;
 
         Command::new("ffmpeg")
             .arg("-i")
-            .arg(video.path())
-            .args(&[
-                "-i", &self.audio,
-                "-shortest",
-                "-c:v:0", "copy",
-                "-y"
-            ])
+            .arg(&video_path)
+            .args(&["-i", &self.audio, "-shortest", "-c:v:0", "copy", "-y"])
             .arg(path)
-            .output()?;
+            .output()
+            .await?;
 
-        video.close()?;
+        video_path.close()?;
         Ok(())
     }
 
-    pub fn download_loops(&self, path: &Path, loops: usize) -> BoxResult<()> {
-        let mut video = NamedTempFile::new()?;
-        self.write_video_to(video.as_file_mut())?;
+    pub async fn download_loops(&self, path: &Path, loops: usize) -> BoxResult<()> {
+        let video_path = NamedTempFile::new()?.into_temp_path();
+        self.write_video_to(&video_path).await?;
 
-        let mut concat_file = NamedTempFile::new()?;
-        let line = format!("file '{}'\n", video.path().display());
-        concat_file.write_all(line.repeat(loops).as_bytes())?;
+        let concat_file_path = NamedTempFile::new()?.into_temp_path();
+        let mut concat_file = File::create(&concat_file_path).await?;
+        let line = format!("file '{}'\n", video_path.display());
+        concat_file.write_all(line.repeat(loops).as_bytes()).await?;
 
         Command::new("ffmpeg")
-            .args(&[
-                "-f", "concat",
-                "-safe", "0",
-                "-i"
-            ])
-            .arg(concat_file.path())
-            .args(&[
-                "-i", &self.audio,
-                "-shortest",
-                "-c:v:0", "copy",
-                "-y"
-            ])
+            .args(&["-f", "concat", "-safe", "0", "-i"])
+            .arg(&concat_file_path)
+            .args(&["-i", &self.audio, "-shortest", "-c:v:0", "copy", "-y"])
             .arg(path)
-            .output()?;
+            .output()
+            .await?;
 
-        video.close()?;
-        concat_file.close()?;
+        video_path.close()?;
+        concat_file_path.close()?;
         Ok(())
     }
 
-    fn write_video_to(&self, file: &mut dyn Write) -> BoxResult<()> {
-        let mut res = reqwest::get(&self.video)?;
-        let mut buf: Vec<u8> = vec![];
-        res.copy_to(&mut buf)?;
-        buf[0] = 0;
-        buf[1] = 0;
-        file.write_all(&buf)?;
+    async fn write_video_to(&self, path: &Path) -> BoxResult<()> {
+        let mut res = reqwest::get(&self.video).await?;
+        let mut file = File::create(&path).await?;
+        let mut first_chunk = true;
+
+        while let Some(mut chunk) = res.chunk().await? {
+            let mut bytes = BytesMut::with_capacity(chunk.len());
+
+            if first_chunk {
+                first_chunk = false;
+                bytes.put_u8(0);
+                bytes.put_u8(0);
+                bytes.put(chunk.split_off(2));
+            } else {
+                bytes.put(chunk);
+            }
+
+            file.write_all(&bytes).await?;
+        }
+
         Ok(())
     }
 }
 
-pub fn fetch_coub(id: &str) -> BoxResult<Coub> {
+pub async fn fetch_coub(id: &str) -> BoxResult<Coub> {
     let id = get_coub_id(id);
     let mut url = "http://coub.com/api/v2/coubs/".to_string();
     url.push_str(&id);
-    let json: Value = reqwest::get(&url)?.json()?;
+    let json: Value = reqwest::get(&url).await?.json().await?;
     let urls = &json["file_versions"]["html5"];
 
     let audio = get_highest_quality(&urls["audio"]);
@@ -92,7 +95,7 @@ pub fn fetch_coub(id: &str) -> BoxResult<Coub> {
         audio: audio["url"].as_str().unwrap().to_string(),
         video: video["url"].as_str().unwrap().to_string(),
         duration: json["duration"].as_f64().unwrap(),
-        size: video["size"].as_u64().unwrap()
+        size: video["size"].as_u64().unwrap(),
     })
 }
 
--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ mod coub;
 use lazy_static::lazy_static;
 use regex::Regex;
 use serenity::{
+    async_trait,
     http::AttachmentType,
     model::{channel::Message, gateway::Ready},
     prelude::*,
 };
-use tempfile::Builder;
 use std::env;
 
 // 8MB file size limit by discord :(
@@ -18,15 +18,20 @@ lazy_static! {
 
 struct Handler;
 
+#[async_trait]
 impl EventHandler for Handler {
-    fn message(&self, ctx: Context, msg: Message) {
+    async fn message(&self, ctx: Context, msg: Message) {
         if let Some(url_match) = COUB_REGEX.find(&msg.content) {
-            let mut status_message = msg.channel_id.say(&ctx.http, "Working on it...").unwrap();
+            let mut status_message = msg
+                .channel_id
+                .say(&ctx.http, "Working on it...")
+                .await
+                .unwrap();
 
-            if let Ok(c) = coub::fetch_coub(url_match.as_str()) {
+            if let Ok(c) = coub::fetch_coub(url_match.as_str()).await {
                 let loops = (MAX_SIZE / c.size) as usize;
 
-                let path = Builder::new()
+                let path = tempfile::Builder::new()
                     .prefix(&c.id)
                     .suffix(".mp4")
                     .rand_bytes(0)
@@ -35,23 +40,26 @@ impl EventHandler for Handler {
                     .into_temp_path();
 
                 let result = if loops < 2 {
-                    c.download(&path)
+                    c.download(&path).await
                 } else {
-                    c.download_loops(&path, loops)
+                    c.download_loops(&path, loops).await
                 };
 
                 match result {
                     Ok(()) => {
-                        let _ = msg.channel_id.send_message(&ctx.http, |m| {
-                            m.add_file(AttachmentType::Path(&path))
-                        });
-                        let _ = status_message.delete(&ctx);
-                    },
+                        let _ = msg
+                            .channel_id
+                            .send_message(&ctx.http, |m| m.add_file(AttachmentType::Path(&path)))
+                            .await;
+                        let _ = status_message.delete(&ctx).await;
+                    }
                     Err(why) => {
                         println!("Error converting coub {}\n{:?}", c.id, why);
-                        let _ = status_message.edit(&ctx, |m| {
-                            m.content(format!("Error converting coub {}\n{:?}", c.id, why))
-                        });
+                        let _ = status_message
+                            .edit(&ctx, |m| {
+                                m.content(format!("Error converting coub {}\n{:?}", c.id, why))
+                            })
+                            .await;
                     }
                 }
 
@@ -60,15 +68,20 @@ impl EventHandler for Handler {
         }
     }
 
-    fn ready(&self, _: Context, ready: Ready) {
+    async fn ready(&self, _: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
     }
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let token = env::var("TOKEN").expect("TOKEN not found in environment.");
-    let mut client = Client::new(&token, Handler).expect("Error creating client");
-    if let Err(why) = client.start() {
+    let mut client = Client::new(&token)
+        .event_handler(Handler)
+        .await
+        .expect("Error creating client");
+
+    if let Err(why) = client.start().await {
         println!("Client error: {:?}", why);
     }
 }
